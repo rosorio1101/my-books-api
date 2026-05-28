0086b66ff5d745881bdad7d81d281624123b527f
Roberto Osorio <robertoosorio.s@gmail.com>
2026-05-27 21:22:43 -0400

    chore: add errors

--- internal/domain/errors.go
+++ internal/domain/errors.go
@@ -1,1 +1,46 @@
 package domain
+
+import (
+	"errors"
+	"fmt"
+)
+
+var ErrBookNotFound = errors.New("Book not found")
+
+var ErrCartEmpty = errors.New("Cart is empty")
+
+var ErrInvalidQuantity = errors.New("Invalid quantity, must be greater than zero")
+
+var ErrAlreadyInFavorites = errors.New("Book is already in favorites")
+
+var ErrFavoriteNotFound = errors.New("Favorite not found")
+
+var ErrItemNotInCart = errors.New("Item not in cart")
+
+type ValidationError struct {
+	Field   string
+	Message string
+}
+
+func (e *ValidationError) Error() string {
+	return fmt.Sprintf("Failed validation on field %s: %s", e.Field, e.Message)
+}
+
+type NotFoundError struct {
+	Resource string
+	ID       string
+}
+
+func (e *NotFoundError) Error() string {
+	return fmt.Sprintf("%s with ID %s not found", e.Resource, e.ID)
+}
+
+func isNotFound(err error) bool {
+	var notFound *NotFoundError
+	return errors.As(err, &notFound)
+}
+
+func isValidationError(err error) bool {
+	var validErr *ValidationError
+	return errors.As(err, &validErr)
+}
